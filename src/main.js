d7a8b4533809711e94737574488f4f0f03631aec
zhongxuetian <642787177@qq.com>
2021-07-31 14:14:31 +0800

    1

--- src/main.js
+++ src/main.js
@@ -11,15 +11,49 @@ import funs from './assets/index.js'
 import * as echarts from 'echarts'
 
 Vue.directive('drag', {
-  bind (el, d) {
-    el.ondragenter = function (ev) {
-      d.value.d.push(1)
-      ev.preventDefault()
-      // var data = ev.dataTransfer.getData('text')
-      // ev.target.appendChild(document.getElementById(data))
+  inserted (el, d) {
+    const oDiv1 = el.parentElement
+    const oMove = el.querySelector('.move')
+    oDiv1.style.position = 'relative'
+    el.style.position = 'absolute'
+    console.log(oDiv1)
+    function getStyle (obj, attr) {
+      if (obj.currentStyle) {
+        return obj.currentStyle[attr]
+      } else {
+        return getComputedStyle(obj, null)[attr]
+      }
     }
-    el.onallowDrop = function (ev) {
-      ev.preventDefault()
+    oMove.onmousedown = function (ev) {
+      var oEvent = ev || event
+      // var disX = oEvent.clientX - el.offsetLeft;
+      // var disY = oEvent.clientY - el.offsetTop;
+      var disX = oEvent.clientX - parseInt(getStyle(el, 'left'))
+      var disY = oEvent.clientY - parseInt(getStyle(el, 'top'))
+
+      document.onmousemove = function (ev) {
+        var oEvent = ev || event
+        var l = oEvent.clientX - disX
+        var t = oEvent.clientY - disY
+
+        if (l < 0) {
+          l = 0
+        } else if (l > oDiv1.offsetWidth - /* parseInt(getStyle(el,'width')) */el.offsetWidth) {
+          l = oDiv1.offsetWidth - el.offsetWidth
+        }
+        if (t < 0) {
+          t = 0
+        } else if (t > oDiv1.offsetHeight - el.offsetHeight) {
+          t = oDiv1.offsetHeight - el.offsetHeight
+        }
+        el.style.left = l + 'px'
+        el.style.top = t + 'px'
+      }
+
+      document.onmouseup = function () {
+        document.onmousemove = null// 如果不取消，鼠标弹起div依旧会随着鼠标移动
+        document.onmouseup = null
+      }
     }
   }
 })
--- src/views/aside/Drag.vue
+++ src/views/aside/Drag.vue
@@ -1,17 +1,84 @@
 <template>
-  <div class="box">
-    <div draggable="true" @dragstart="drag($event)">可拖拽</div>
-    <div class="b" v-drag='{d:list}' >
-      <p v-for="item in list" :key='item'>{{ item }}</p>
+    <div class="container">
+        <div class="title">
+            <draggable
+                    v-model="list"
+                    group="site"
+                    animation="300"
+                    dragClass="dragClass"
+                    ghostClass="ghostClass"
+                    chosenClass="chosenClass"
+                    @start="onStart"
+                    @end="onEnd"
+                >
+                    <transition-group>
+                        <div class="item" v-for="item in list" :key="item.id">{{item.name}}</div>
+                    </transition-group>
+                </draggable>
+        </div>
+        <div class="box">
+            <!-- <div draggable="true" @dragstart="drag($event)">可拖拽</div> -->
+            <div class="b" v-drag>
+                <div class="move">移动</div>
+                <draggable
+                    v-model="arr1"
+                    group="site"
+                    animation="300"
+                    dragClass="dragClass"
+                    ghostClass="ghostClass"
+                    chosenClass="chosenClass"
+                    @start="onStart"
+                    @end="onEnd"
+                >
+                    <transition-group>
+                        <div class="item" v-for="item in arr1" :key="item.id">{{item.name}}</div>
+                    </transition-group>
+                </draggable>
+            </div>
+            <div class="b" v-drag>
+                <div class="move">移动</div>
+                <draggable
+                    v-model="arr2"
+                    group="site"
+                    animation="300"
+                    dragClass="dragClass"
+                    ghostClass="ghostClass"
+                    chosenClass="chosenClass"
+                    @start="onStart"
+                    @end="onEnd"
+                >
+                    <transition-group>
+                        <div class="item" v-for="item in arr2" :key="item.id">{{item.name}}</div>
+                    </transition-group>
+                </draggable>
+            </div>
+        </div>
     </div>
-    <div class="b"></div>
-  </div>
 </template>
 <script>
+import draggable from 'vuedraggable'
 export default {
   data () {
     return {
-      list: [1]
+      drag: false,
+      list: [
+        { id: 11, name: 'www.11.com' },
+        { id: 12, name: 'www.22.com' },
+        { id: 13, name: 'www.33.com' },
+        { id: 14, name: 'www.44.com' }
+      ],
+      arr1: [
+        { id: 1, name: 'www.itxst.com' },
+        { id: 2, name: 'www.jd.com' },
+        { id: 3, name: 'www.baidu.com' },
+        { id: 4, name: 'www.taobao.com' }
+      ],
+      arr2: [
+        { id: 5, name: 'www.google.com' },
+        { id: 6, name: 'www.msn.com' },
+        { id: 7, name: 'www.ebay.com' },
+        { id: 8, name: 'www.yahoo.com' }
+      ]
     }
   },
   created () {},
@@ -19,25 +86,91 @@ export default {
     // allowDrop (ev) {
     //   ev.preventDefault()
     // },
-    drag (ev, d) {
-      ev.dataTransfer.setData('text', d)
-    }
+    // drag (ev, d) {
+    //   ev.dataTransfer.setData('text', d)
+    // },
     // drop (ev) {
     //   ev.preventDefault()
     //   //   var data = ev.dataTransfer.getData('text')
     //   //   ev.target.appendChild(document.getElementById(data))
     // }
+    // 开始拖拽事件
+    onStart () {
+      this.drag = true
+    },
+    // 拖拽结束事件
+    onEnd () {
+      this.drag = false
+    }
+
+  },
+  components: {
+    draggable
   }
 }
 </script>
 <style lang="less" scoped>
+.container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
 .box {
-  background: #eee;
-  height: 100%;
-  .b {
-    height: 200px;
-    width: 200px;
-    border: 1px solid red;
-  }
+    background: #eee;
+    height: 100%;
+    flex-grow: 1;
+    .b {
+        height: 200px;
+        width: 200px;
+        border: 1px solid red;
+        background: red;
+    }
+}
+.ghostClass{
+  background-color:  blue !important;
+}
+.chosenClass{
+  background-color: red !important;
+  opacity: 1!important;
+}
+.dragClass{
+  background-color: blueviolet !important;
+  opacity: 1 !important;
+  box-shadow:none !important;
+  outline:none !important;
+  background-image:none !important;
+}
+.itxst{
+  margin: 10px;
+
+}
+.title{
+  padding: 6px 12px;
+}
+.col{
+  width: 40%;
+  flex: 1;
+  padding: 10px;
+  border: solid 1px #eee;
+  border-radius:5px ;
+  float: left;
+}
+.col+.col{
+ margin-left: 10px;
+}
+
+.item{
+  padding: 6px 12px;
+  margin: 0px 10px 0px 10px;
+  border:  solid 1px #eee;
+   background-color: #f1f1f1;
+}
+.item:hover{
+  background-color: #fdfdfd;
+  cursor: move;
+}
+.item+.item{
+  border-top:none ;
+  margin-top: 6px;
 }
 </style>
